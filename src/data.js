4bb4daade5a3873aaae9cd32622a98fb3220ce68
boltex33 <laca_rey@yahoo.com>
2022-01-30 21:23:35 +0200

    push-1

---
+++ src/data.js
@@ -0,0 +1,99 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Snaring River",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/09/1db4d2274604636097241aca0f7077594e95edc7-300x300.jpg",
+      artist: "Moods",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=24694",
+      color: ["#318C79", "#C6DDE5"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Flowers",
+      artist: "The Filed Tapes, xander",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11249",
+    },
+    {
+      name: "Going Back",
+      artist: "Swørn",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+    },
+    {
+      name: "Driftaway",
+      artist: "Tesk, No Spiri, Sitting Duck",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9175",
+    },
+    {
+      name: "Bliss",
+      artist: "Misha, Jussi Halme",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/b6e48e6cfb2a90723b9cf1b108a6d305f9204eb4-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+    },
+    {
+      name: "Turbulance",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-300x300.jpg",
+      artist: "Parkbench Epiphany",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27503",
+      color: ["#1F2025", "#354451"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Seascape",
+      artist: "Makzo",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
+    },
+    {
+      name: "Small Town Palm Trees",
+      artist: "Mama Aiuto",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FBAC6C", "#0952B6"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+    },
+
+    {
+      name: "la zona",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/3ff29a35be582c8dc0222273cb9c401ea6b209dc-300x300.jpg",
+      artist: "Maydee",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27455",
+      color: ["#B0C1D7", "#6D6042"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
